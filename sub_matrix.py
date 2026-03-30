52314c1482a62954c0d32223aa2bcf7f10cc5092
L.Akshay <ladiakshay27@gmail.com>
2026-03-30 14:09:03 +0530

    Add matrix subtraction function

---
+++ sub_matrix.py
@@ -0,0 +1,12 @@
+def matrix_subtract(A_str, B_str):
+    A = parse_matrix(A_str)
+    B = parse_matrix(B_str)
+
+    validate_same_dimension(A, B)
+
+    result = [
+        [A[i][j] - B[i][j] for j in range(len(A[0]))]
+        for i in range(len(A))
+    ]
+
+    return str(result)
